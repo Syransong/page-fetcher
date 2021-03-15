ca8266dae00bcfef3b87a7a3a739dde73a5dfba3
Karina Song <ksysong@gmail.com>
2021-03-15 05:16:44 +0000

    added code and tested if the given URL results in an error/non-200 result

--- fetcher.js
+++ fetcher.js
@@ -1,14 +1,3 @@
-// a small command line node app which takes a URL as a command-line argument as well 
-// as a local file path and downloads the resource to the specified path. 
-
-// 4. Allow user to respond to app using terminal stdin (readline)
-
-//edge cases: 
-  // what if local file path already exists? -> overwrite file without prompting
-  // what if local file path given is invalid? -> check if its valid, otherwise fail and let user know
-      // create if statement to check if the file path is valid or not 
-  // what happens if the given URL results in an error or non-200 result? -> terminate the app, inform the user and do not write 
-
 const request = require('request');
 const fs = require('fs');
 const readline = require('readline');
@@ -29,7 +18,12 @@ if (!isValid(filePath)) {
 
 } else {
   request(url, (error, response, body) => {
-  
+
+    if (error || response.statusCode !== 200) {
+      rl.close();
+      return console.log("Seems like that page doesn't exist or something :( Please double check and try again!");
+    }
+
     fs.writeFile(filePath, body, (error) => {
       if (error) {
         return console.log("Error, didn't print", error);
